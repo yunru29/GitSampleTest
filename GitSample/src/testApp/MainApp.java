d6ef5d7c7a37f613b499b9017e5a3f42569f5e35
Carol <yunru29@gmail.com>
2022-11-05 14:51:59 +0800

    3th Version

--- GitSample/src/testApp/MainApp.java
+++ GitSample/src/testApp/MainApp.java
@@ -4,5 +4,16 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("2nd Version");
+		
+		
+		//組長新增的code
+		Math cMatm=new Math();
+		System.out.println();
+		System.out.print(cMatm.add(5, 5));
+	}
+}
+class Math{
+	int add(int x,int y) {
+		return x+y;
 	}
 }
